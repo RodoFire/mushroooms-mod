f9d10708a5f0f0cac0653951b8abc04aa08e97fb
RodoFire <louisbriche@gmail.com>
2025-01-16 11:59:34 +0100

    minor entities modifications

--- src/main/java/net/rodofire/mushrooomsmod/entity/custom/CrystalGolemEntity.java
+++ src/main/java/net/rodofire/mushrooomsmod/entity/custom/CrystalGolemEntity.java
@@ -30,8 +30,8 @@ import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 
 import java.util.List;
